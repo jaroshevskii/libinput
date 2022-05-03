2d97080d1ec5aea71929fd30050be9f6d9af5559
jaroshevskii <jaroshevskiisasha@gmail.com>
2022-05-03 21:39:32 +0300

    Update

--- LibInput.cpp
+++ LibInput.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string_view>
 
 #include "Config.h"
 
 int main() {
-  std::cout << "LibInput" << ' ' << version::major << '.' << version::minor
-            << '.' << version::patch << '\n';
+  std::cout << project::name << ' ' << project::version::major << '.'
+            << project::version::minor << '.' << project::version::patch
+            << '\n';
+  return 0;
 }
